135b3d059afd506cc0fcdb999db61e26e5de4a08
Erik L Olson <eriklolson@zohomail.com>
2023-01-17 15:48:43 -0800

    removed unused files

--- flask_wineshop/products/helpers.py
+++
@@ -1,8 +0,0 @@
-
-from sqlalchemy import desc
-from flask_wineshop.models import *
-from flask_login import current_user
-
-
-def get_favorites():
-    favorites = db.session(Bottles).order_by(desc(Bottles.description)).all()
